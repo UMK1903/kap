d670c78f8d091fe1b46923c11b6f14e0ed6876a1
UMK1903 <79329005+UMK1903@users.noreply.github.com>
2021-02-20 03:59:31 +0300

    Update README.py

--- README.py
+++ README.py
@@ -1,3 +1,40 @@
+#CSS ile de filtreleyebiliriz, ben bunu tercih ediyorum.
+sozler=icerigiAl.select("div#PageContent")
+for i in range(0,len(sozler)): #döngüyü, aldığımız dönütün uzunluğu ile sınırlandırıyoruz.
+print(sozler[i].contents[0].text +": "+sozler[i].contents[2]+"\n") #Etiket içeriklerini çekip yazdırıyoruz.
+
+import requests
+from bs4 import BeautifulSoup
+url="http://guzelsozlerblog.com/gunun-sozu"
+response=requests.get(url)
+<span style="color:#ff0000;">response.encoding= "iso-8859-9"</span> #Bu kod ile çektiğimiz verideki "ş,ç,i" gibi karakterlerde çıkabilecek problemleri önlüyoruz.
+AnalizEdilecekIcerik=response.text
+icerigiAl=BeautifulSoup(AnalizEdilecekIcerik,"lxml")
+
+
+print(filtre) #tüm filtreyi yazdırdık
+print(filtre[0]) #filtreden gelen verinin sadece 0.indexine ait içe
+
+filtre=icerigiAl.find_all(id="PageContent") #id değeriyle filtreleme yaptık
+
+icerigiAl=BeautifulSoup(AnalizEdilecekIcerik,"lxml") #BS4 HTML kodunu analiz ediyor
+#"lxml" parametresini kütüphanenin bize verdiği uyarı gereği kullandık. Parametresiz veya diğer parametreler ile kullanabilirsiniz. Bu parametre hakkında daha fazla bilgi için--&gt; https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
+
+
+import requests
+from bs4 import BeautifulSoup
+url="http://guzelsozlerblog.com/gunun-sozu"
+response=requests.get(url)
+print(response.text)
+AnalizEdilecekIcerik=response.text #Sayfadan gelen HTML kodunu atadık
+
+
+import requests #requests kütühanemizi dahil ettik
+url="http://guzelsozlerblog.com/gunun-sozu" #web sayfası URL sini atadık
+response=requests.get(url) #kütüphane yardımıyla web sayfasına gittik
+print(response.text) #web sayfasının çıktısını yazdırdık
+
+
 import requests
 from bs4 import BeautifulSoup
 
